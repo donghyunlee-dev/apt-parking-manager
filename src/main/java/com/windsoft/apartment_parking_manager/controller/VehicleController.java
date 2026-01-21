fb593b639445d97a5dd079581cd755f084a89607
donghyun lee <80296014+donghyunlee-dev@users.noreply.github.com>
2026-01-21 18:12:19 +0900

    Add methods for resident and visit vehicles

--- src/main/java/com/windsoft/apartment_parking_manager/controller/VehicleController.java
+++ src/main/java/com/windsoft/apartment_parking_manager/controller/VehicleController.java
@@ -1,7 +1,10 @@
 package com.windsoft.apartment_parking_manager.controller;
 
 import com.windsoft.apartment_parking_manager.data.entity.ParkingVehicle;
+import com.windsoft.apartment_parking_manager.data.entity.ResidentVehicle;
+import com.windsoft.apartment_parking_manager.data.entity.VisitVehicle;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +15,26 @@ public class VehicleController {
 
     @PostMapping("/parking")
     public ResponseEntity<?> recordParkingVehicle(final ParkingVehicle vehicle) {
-
         return ResponseEntity.ok(vehicle);
+    }
+
+    @PostMapping("/resident")
+    public ResponseEntity<?> addResidentVehicle(final ResidentVehicle vehicle) {
+        return ResponseEntity.ok(vehicle);
+    }
+
+    @PostMapping("/visit")
+    public ResponseEntity<?> addVisitVehicle(final VisitVehicle vehicle) {
+        return ResponseEntity.ok(vehicle);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> retrieveVehicles(final Integer vehicleId) {
+        return ResponseEntity.ok(vehicleId);
+    }
+
+    @GetMapping("/{no}")
+    public ResponseEntity<?> retrieveVehicleById(final String vehicleNo) {
+        return ResponseEntity.ok(vehicleNo);
     }
 }
