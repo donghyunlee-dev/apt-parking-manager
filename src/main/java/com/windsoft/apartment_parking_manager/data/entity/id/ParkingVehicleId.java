38e92a18d2594aba88d6f1ceb2f3864baf549ebe
donghyun lee <80296014+donghyunlee-dev@users.noreply.github.com>
2026-01-21 17:19:52 +0900

    Add files via upload

---
+++ src/main/java/com/windsoft/apartment_parking_manager/data/entity/id/ParkingVehicleId.java
@@ -0,0 +1,32 @@
+package com.windsoft.apartment_parking_manager.data.entity.id;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@EqualsAndHashCode
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ParkingVehicleId implements Serializable {
+    public static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "parking_vehicle_seq"
+    )
+    @SequenceGenerator(
+            name = "parking_vehicle_seq",
+            sequenceName = "parking_vehicle_seq",
+            allocationSize = 1
+    )
+    private long seqNo;
+}
