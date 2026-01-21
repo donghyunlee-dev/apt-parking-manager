0da3cc67a1ec91db3967062c0f7363aa0c5e3dc4
donghyun lee <80296014+donghyunlee-dev@users.noreply.github.com>
2026-01-21 16:50:20 +0900

    Refactor ApartmentResponseDto methods
    Renamed method to setData and added noData method.

--- src/main/java/com/windsoft/apartment_parking_manager/data/dto/ApartmentResponseDto.java
+++ src/main/java/com/windsoft/apartment_parking_manager/data/dto/ApartmentResponseDto.java
@@ -16,13 +16,23 @@ public class ApartmentResponseDto {
 
     private Integer residentCount;
 
-    public static ApartmentResponseDto toDto(Apartment apartment) {
+    public static ApartmentResponseDto setData(Apartment apartment) {
         ApartmentResponseDto response = new ApartmentResponseDto();
         response.code = apartment.getAptCode();
         response.name = apartment.getAptName();
         response.address = apartment.getAddress();
         response.buildingCount = apartment.getBuilding();
         response.residentCount = apartment.getResident();
+        return response;
+    }
+
+    public static ApartmentResponseDto noData() {
+        ApartmentResponseDto response = new ApartmentResponseDto();
+        response.code = "";
+        response.name = "";
+        response.address = "";
+        response.buildingCount = 0;
+        response.residentCount = 0;
         return response;
     }
 }
