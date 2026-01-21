19419bacc337193d523ad6e700ea93edae976b66
donghyun lee <80296014+donghyunlee-dev@users.noreply.github.com>
2026-01-21 17:00:21 +0900

    Add ResidentVehicle entity class
    입주민 차량 Entity 추가

---
+++ src/main/java/com/windsoft/apartment_parking_manager/data/entity/ResidentVehicle.java
@@ -0,0 +1,27 @@
+package com.windsoft.apartment_parking_manager.data.entity;
+
+import com.windsoft.apartment_parking_manager.data.entity.id.ResidentVehicleId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Getter
+@IdClass(ResidentVehicleId.class)
+@Table(name = "resident_vehicle")
+@Entity
+public class ResidentVehicle extends BaseEntity {
+    @Id
+    private String aptCode;
+    @Id
+    private String vehicleNo;
+
+    private String bdId;
+
+    private String bdLoc;
+
+    private String phone;
+
+    private boolean used;
+}
