a3d2aad62d698cb1386f25c5b92bb1c00d7daa88
donghyun lee <80296014+donghyunlee-dev@users.noreply.github.com>
2026-01-22 13:48:12 +0900

    Add files via upload

--- src/main/java/com/windsoft/apartment_parking_manager/service/VehicleService.java
+++ src/main/java/com/windsoft/apartment_parking_manager/service/VehicleService.java
@@ -2,8 +2,11 @@ package com.windsoft.apartment_parking_manager.service;
 
 import com.windsoft.apartment_parking_manager.data.dto.VehicleRequestDto;
 import com.windsoft.apartment_parking_manager.data.dto.VehicleResponseDto;
+import com.windsoft.apartment_parking_manager.data.entity.ParkingVehicle;
 
 public interface VehicleService {
 
     VehicleResponseDto findParkingVehicle(VehicleRequestDto.ParkingRequest request);
+
+    ParkingVehicle saveParkingVehicle(VehicleRequestDto.ParkingRequest request, VehicleResponseDto vehicleInfo);
 }
