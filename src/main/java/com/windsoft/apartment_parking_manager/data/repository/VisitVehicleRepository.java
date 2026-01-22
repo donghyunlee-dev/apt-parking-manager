2fa600a6d3a27af7aef6c158a9499addace3bd3c
donghyun lee <80296014+donghyunlee-dev@users.noreply.github.com>
2026-01-22 14:48:29 +0900

    Add files via upload

--- src/main/java/com/windsoft/apartment_parking_manager/data/repository/VisitVehicleRepository.java
+++ src/main/java/com/windsoft/apartment_parking_manager/data/repository/VisitVehicleRepository.java
@@ -4,5 +4,9 @@ import com.windsoft.apartment_parking_manager.data.entity.VisitVehicle;
 import com.windsoft.apartment_parking_manager.data.entity.id.VisitVehicleId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 public interface VisitVehicleRepository extends JpaRepository<VisitVehicle, VisitVehicleId> {
+    Optional<VisitVehicle> findByAptCodeAndVehicleNoAndVisitDateLessThanEqualAndVisitCloseDateGreaterThanEqual(String aptCode, String vehicleNo, LocalDate from, LocalDate to);
 }
