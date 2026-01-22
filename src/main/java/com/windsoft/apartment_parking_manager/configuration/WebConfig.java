ebb39838ba678b9195cf0d47833efb12414a59ce
donghyun lee <80296014+donghyunlee-dev@users.noreply.github.com>
2026-01-22 10:44:49 +0900

    Add files via upload

---
+++ src/main/java/com/windsoft/apartment_parking_manager/configuration/WebConfig.java
@@ -0,0 +1,23 @@
+package com.windsoft.apartment_parking_manager.configuration;
+
+import com.windsoft.apartment_parking_manager.controller.RequestContextArgumentResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final RequestContextArgumentResolver resolver;
+
+    public WebConfig(RequestContextArgumentResolver resolver) {
+        this.resolver = resolver;
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(resolver);
+    }
+}
