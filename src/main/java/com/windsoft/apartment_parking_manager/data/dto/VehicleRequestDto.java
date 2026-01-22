149ea4d80e48c10d550ff9494c80eebc328d8052
donghyun lee <80296014+donghyunlee-dev@users.noreply.github.com>
2026-01-22 12:26:01 +0900

    Add files via upload

--- src/main/java/com/windsoft/apartment_parking_manager/data/dto/VehicleRequestDto.java
+++ src/main/java/com/windsoft/apartment_parking_manager/data/dto/VehicleRequestDto.java
@@ -1,7 +1,21 @@
 package com.windsoft.apartment_parking_manager.data.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+@AllArgsConstructor
+@Getter
 public class VehicleRequestDto {
+
+    @Getter
+    public static class ParkingRequest extends RequestContextDto {
+
+        private String vehicleNo;
+
+        public ParkingRequest(RequestContext context, final String vehicleNo) {
+            super(context);
+            this.vehicleNo = vehicleNo;
+        }
+    }
 
 }
