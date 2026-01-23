920de4c21a951d41c9b55315c352f8887b853631
donghyun lee <80296014+donghyunlee-dev@users.noreply.github.com>
2026-01-23 17:10:50 +0900

    Refactor password encoding test for better readability

--- src/test/java/util/PasswordUtils.java
+++ src/test/java/util/PasswordUtils.java
@@ -1,15 +1,23 @@
 package util;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.text.MessageFormat;
 
 public class PasswordUtils {
 
     @Test
     void createPasswordBcrypt() {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        String hash = encoder.encode("123456");
-        System.out.println("hash "+ hash);
+        String testPassword = "123456";
 
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String hash = encoder.encode(testPassword);
+        System.out.println(MessageFormat.format("hash : {0}", hash));
+
+        boolean matches = encoder.matches(testPassword, hash);
+
+        Assertions.assertTrue(matches);
     }
 }
