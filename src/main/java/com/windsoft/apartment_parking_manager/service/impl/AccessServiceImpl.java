05dd632324dbe3aca2335019697e86aeee14ae59
donghyun lee <80296014+donghyunlee-dev@users.noreply.github.com>
2026-01-23 17:13:08 +0900

    Replace PasswordEncoder with BcryptUtils for validation

--- src/main/java/com/windsoft/apartment_parking_manager/service/impl/AccessServiceImpl.java
+++ src/main/java/com/windsoft/apartment_parking_manager/service/impl/AccessServiceImpl.java
@@ -7,9 +7,9 @@ import com.windsoft.apartment_parking_manager.data.entity.Bouncer;
 import com.windsoft.apartment_parking_manager.data.repository.AccessLogRepository;
 import com.windsoft.apartment_parking_manager.data.repository.BouncerRepository;
 import com.windsoft.apartment_parking_manager.service.AccessService;
+import com.windsoft.apartment_parking_manager.util.BcryptUtils;
 import com.windsoft.apartment_parking_manager.util.CustomObjectUtils;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -21,7 +21,6 @@ public class AccessServiceImpl implements AccessService {
 
     private final AccessLogRepository accessLogRepository;
     private final BouncerRepository bouncerRepository;
-    private final PasswordEncoder encoder;
 
     @Override
     public AccessResponseDto validateAccess(AccessRequestDto requestDto) {
@@ -35,7 +34,7 @@ public class AccessServiceImpl implements AccessService {
         Bouncer findBouncer = null;
 
         for (Bouncer bouncer : bouncers) {
-            if (encoder.matches(requestDto.getFinCode(), bouncer.getFinNo())) {
+            if (BcryptUtils.isValidPassword(requestDto.getFinCode(), bouncer.getFinNo())) {
                 findBouncer = CustomObjectUtils.deepCopy(bouncer, Bouncer.class);
             }
         }
