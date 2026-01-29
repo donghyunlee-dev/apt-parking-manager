0c28ed19d8f2c264ee2dac9f181c81935ad80e34
donghyun lee <80296014+donghyunlee-dev@users.noreply.github.com>
2026-01-29 17:20:05 +0900

    Enhance VehicleInfo with additional data handling methods
    Added methods to set data for VisitorVehicle and handle illegal vehicles.

--- src/main/java/com/windsoft/apartment_parking_manager/data/dto/VehicleResponseDto.java
+++ src/main/java/com/windsoft/apartment_parking_manager/data/dto/VehicleResponseDto.java
@@ -1,6 +1,7 @@
 package com.windsoft.apartment_parking_manager.data.dto;
 
 import com.windsoft.apartment_parking_manager.data.entity.ResidentVehicle;
+import com.windsoft.apartment_parking_manager.data.entity.VisitorVehicle;
 import com.windsoft.apartment_parking_manager.type.VehicleType;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,9 +16,7 @@ public class VehicleResponseDto {
     @Setter
     public static class ParkingInfo {
         private String vehicleNo;
-
         private String info;
-
         private VehicleType status;
     }
 
@@ -28,6 +27,7 @@ public class VehicleResponseDto {
         private String bdId;
         private String bdUnit;
         private String phone;
+        private VehicleType status;
 
         private VehicleInfo() {}
 
@@ -38,7 +38,31 @@ public class VehicleResponseDto {
             vehicleInfo.bdId = residentVehicle.getBdId();
             vehicleInfo.bdUnit = residentVehicle.getBdUnit();
             vehicleInfo.phone = residentVehicle.getPhone();
+            vehicleInfo.status = VehicleType.RESIDENT;
             return vehicleInfo;
+        }
+
+        public static VehicleInfo setData(VisitorVehicle visitorVehicle) {
+            VehicleInfo vehicleInfo = new VehicleInfo();
+            vehicleInfo.aptCode = visitorVehicle.getAptCode();
+            vehicleInfo.vehicleNo = visitorVehicle.getVehicleNo();
+            vehicleInfo.bdId = visitorVehicle.getBdId();
+            vehicleInfo.bdUnit = visitorVehicle.getBdUnit();
+            vehicleInfo.phone = visitorVehicle.getPhone();
+            vehicleInfo.status = VehicleType.VISITOR;
+            return vehicleInfo;
+        }
+
+        public static VehicleInfo setData(String aptCode, String vehicleNo) {
+            VehicleInfo vehicleInfo = new VehicleInfo();
+            vehicleInfo.aptCode = aptCode;
+            vehicleInfo.vehicleNo = vehicleNo;
+            vehicleInfo.status = VehicleType.ILLEGAL;
+            return vehicleInfo;
+        }
+
+        public static VehicleInfo noData() {
+            return new VehicleInfo();
         }
     }
 }
