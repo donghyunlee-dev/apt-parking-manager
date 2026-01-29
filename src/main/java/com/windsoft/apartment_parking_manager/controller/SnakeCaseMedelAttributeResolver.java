f51bf8eac60bae58a40c771a4e2a0f9f297fd68e
donghyun lee <80296014+donghyunlee-dev@users.noreply.github.com>
2026-01-29 10:42:10 +0900

    Add files via upload

---
+++ src/main/java/com/windsoft/apartment_parking_manager/controller/SnakeCaseMedelAttributeResolver.java
@@ -0,0 +1,42 @@
+package com.windsoft.apartment_parking_manager.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.windsoft.apartment_parking_manager.annotation.SnakCaseModelAttribute;
+import org.jspecify.annotations.Nullable;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class SnakeCaseMedelAttributeResolver implements HandlerMethodArgumentResolver {
+
+    private final ObjectMapper objectMapper = new ObjectMapper()
+            .setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)
+            .registerModule(new JavaTimeModule());
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(SnakCaseModelAttribute.class);
+    }
+
+    @Override
+    public @Nullable Object resolveArgument(
+            MethodParameter parameter,
+            @Nullable ModelAndViewContainer mavContainer,
+            NativeWebRequest webRequest,
+            @Nullable WebDataBinderFactory binderFactory) throws Exception {
+
+        Map<String, String[]> parameterMap = webRequest.getParameterMap();
+        Map<String, Object> flatMap = new HashMap<>();
+        parameterMap.forEach((k, v) -> flatMap.put(k, v[0]));
+        return objectMapper.convertValue(flatMap, parameter.getParameterType());
+    }
+}
