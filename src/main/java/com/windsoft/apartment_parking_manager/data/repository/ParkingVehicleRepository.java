4aa4ac20077b6f9fc28fc70b3d1d6aaacffc1dc1
donghyun lee <80296014+donghyunlee-dev@users.noreply.github.com>
2026-01-29 17:21:14 +0900

    Add method to find ParkingVehicle by aptCode and vehicleNo
    Added method to find ParkingVehicle by apartment code and vehicle number.

--- src/main/java/com/windsoft/apartment_parking_manager/data/repository/ParkingVehicleRepository.java
+++ src/main/java/com/windsoft/apartment_parking_manager/data/repository/ParkingVehicleRepository.java
@@ -4,5 +4,8 @@ import com.windsoft.apartment_parking_manager.data.entity.ParkingVehicle;
 import com.windsoft.apartment_parking_manager.data.entity.id.ParkingVehicleId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ParkingVehicleRepository extends JpaRepository<ParkingVehicle, ParkingVehicleId> {
+    List<ParkingVehicle> findByAptCodeAndVehicleNo(String aptCode, String vehicleNo);
 }
