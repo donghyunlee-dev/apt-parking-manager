7c77cf3c632018e3e46754ac99513c7a2b5fbaed
donghyun lee <80296014+donghyunlee-dev@users.noreply.github.com>
2026-02-10 17:10:17 +0900

    Add files via upload

--- src/main/java/com/windsoft/apartment_parking_manager/data/entity/Bouncer.java
+++ src/main/java/com/windsoft/apartment_parking_manager/data/entity/Bouncer.java
@@ -1,11 +1,10 @@
 package com.windsoft.apartment_parking_manager.data.entity;
 
 
+import com.windsoft.apartment_parking_manager.data.dto.BouncerRequestDto;
 import com.windsoft.apartment_parking_manager.data.entity.id.BouncerId;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.IdClass;
-import jakarta.persistence.Table;
+import com.windsoft.apartment_parking_manager.util.AES256GcmConverter;
+import jakarta.persistence.*;
 import lombok.Getter;
 
 @Getter
@@ -22,6 +21,7 @@ public class Bouncer extends BaseEntity {
 
     private String bouncerName;
 
+    @Convert(converter = AES256GcmConverter.class)
     private String finNo;
 
     private String grade;
@@ -29,5 +29,18 @@ public class Bouncer extends BaseEntity {
     private String deviceId;
 
     private String used;
+
+    public static Bouncer toEntity(BouncerRequestDto.Registration admin) {
+        Bouncer bouncer = new Bouncer();
+
+        bouncer.aptCode = admin.getAptCode();
+        bouncer.bouncerCode = "B000";
+        bouncer.bouncerName = "관리자";
+        bouncer.finNo = admin.getFinNo();
+        bouncer.grade = "A";
+        bouncer.deviceId = admin.getDeviceId();
+        bouncer.used = "Y";
+        return bouncer;
+    }
 
 }
