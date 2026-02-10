57913d72303991b80d26a7a6bf9085842282d5a4
donghyun lee <80296014+donghyunlee-dev@users.noreply.github.com>
2026-02-10 17:09:09 +0900

    Add files via upload

--- src/main/java/com/windsoft/apartment_parking_manager/controller/ApartmentController.java
+++ src/main/java/com/windsoft/apartment_parking_manager/controller/ApartmentController.java
@@ -1,12 +1,13 @@
 package com.windsoft.apartment_parking_manager.controller;
 
-import com.windsoft.apartment_parking_manager.data.dto.ApartmentRequestDto;
-import com.windsoft.apartment_parking_manager.data.dto.ApartmentResponseDto;
+import com.windsoft.apartment_parking_manager.data.dto.*;
 import com.windsoft.apartment_parking_manager.data.entity.Apartment;
 import com.windsoft.apartment_parking_manager.service.ApartmentService;
+import com.windsoft.apartment_parking_manager.service.BouncerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -15,20 +16,36 @@ import org.springframework.web.bind.annotation.*;
 public class ApartmentController {
 
     private final ApartmentService apartmentService;
+    private final BouncerService bouncerService;
 
     @PostMapping
-    public ResponseEntity<?> registerApartment(@RequestBody ApartmentRequestDto.Registration requestDto) {
-        Apartment apartment = apartmentService.createApartment(requestDto);
+    public ResponseEntity<BaseResponseDto<?>> registerApartment(@RequestBody ApartmentRequestDto.Registration requestDto) {
+
+        Apartment apartment = apartmentService.saveApartment(requestDto);
 
         ApartmentResponseDto response;
 
         if (apartment == null) {
             response = ApartmentResponseDto.noData();
         } else {
-            response = ApartmentResponseDto.setData(apartment);
+            BouncerRequestDto.Registration adminBouncer = BouncerRequestDto.Registration
+                    .builder()
+                    .aptCode(apartment.getAptCode())
+                    .finNo(requestDto.getFinNo())
+                    .deviceId(requestDto.getDeviceId())
+                    .build();
+
+            BouncerResponseDto bouncerResponseDto = bouncerService.saveBouncer(adminBouncer);
+
+            if (ObjectUtils.isEmpty(bouncerResponseDto)) {
+                apartmentService.forceDeleteApartment(apartment.getAptCode());
+                response = ApartmentResponseDto.noData();
+            } else {
+                response = ApartmentResponseDto.setData(apartment);
+            }
         }
 
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        return ResponseEntity.ok(new BaseResponseDto<>(response));
     }
 
     @GetMapping
