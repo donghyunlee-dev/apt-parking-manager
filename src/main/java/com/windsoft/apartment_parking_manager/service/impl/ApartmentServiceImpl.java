b67b47a77a8c3a3d98c2a8686f9732aa9ec4b06f
donghyun lee <80296014+donghyunlee-dev@users.noreply.github.com>
2026-02-10 14:12:38 +0900

    Add files via upload

--- src/main/java/com/windsoft/apartment_parking_manager/service/impl/ApartmentServiceImpl.java
+++ src/main/java/com/windsoft/apartment_parking_manager/service/impl/ApartmentServiceImpl.java
@@ -1,10 +1,12 @@
 package com.windsoft.apartment_parking_manager.service.impl;
 
+import com.windsoft.apartment_parking_manager.data.dto.ApartmentRequestDto;
 import com.windsoft.apartment_parking_manager.data.entity.Apartment;
 import com.windsoft.apartment_parking_manager.data.repository.ApartmentRepository;
 import com.windsoft.apartment_parking_manager.service.ApartmentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.ObjectUtils;
 
 import java.util.Comparator;
@@ -17,8 +19,29 @@ public class ApartmentServiceImpl implements ApartmentService {
     private final ApartmentRepository apartmentRepository;
 
     @Override
-    public Apartment retriveApartmentByAddress(String address) {
+    public Apartment retrieveApartmentByAddress(String address) {
         return apartmentRepository.findByAddressAndStatus(address, "ACTIVE");
+    }
+
+    @Transactional
+    @Override
+    public Apartment createApartment(ApartmentRequestDto.Registration requestDto) {
+
+        String maxApartmentCode = apartmentRepository.getMaxApartmentCode();
+        StringBuilder newCode = new StringBuilder();
+        newCode.append("A");
+
+        try {
+            int splitSequence = Integer.parseInt(maxApartmentCode.substring(1, maxApartmentCode.length()));
+            newCode.append(String.format("%04d", ++splitSequence));
+        } catch (NumberFormatException e) {
+            newCode.append("0000");
+            e.printStackTrace();
+        }
+
+        Apartment apartment = Apartment.newInstance(newCode.toString(), requestDto);
+
+        return apartmentRepository.save(apartment);
     }
 
     public Apartment registerApartment(Apartment apartment) {
