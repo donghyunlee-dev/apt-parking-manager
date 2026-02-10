0c3e349c93fe23762978a41cbb9cb69296aae9a2
donghyun lee <80296014+donghyunlee-dev@users.noreply.github.com>
2026-02-10 14:10:05 +0900

    Add files via upload

--- src/main/java/com/windsoft/apartment_parking_manager/data/dto/ApartmentRequestDto.java
+++ src/main/java/com/windsoft/apartment_parking_manager/data/dto/ApartmentRequestDto.java
@@ -1,13 +1,28 @@
 package com.windsoft.apartment_parking_manager.data.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 public class ApartmentRequestDto {
 
     @AllArgsConstructor
     @Getter
-    public class verify {
+    public class Verification {
         private String address;
+    }
+
+    @Builder
+    @AllArgsConstructor
+    @Getter
+    public static class Registration {
+
+        private String aptName;
+
+        private String address;
+
+        private int building;
+
+        private int resident;
     }
 }
