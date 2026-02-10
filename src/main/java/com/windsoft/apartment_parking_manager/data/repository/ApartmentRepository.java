1eb804ed8fbeaa3a36b956e64c8346f5b3dfe864
donghyun lee <80296014+donghyunlee-dev@users.noreply.github.com>
2026-02-10 14:11:17 +0900

    Add files via upload

--- src/main/java/com/windsoft/apartment_parking_manager/data/repository/ApartmentRepository.java
+++ src/main/java/com/windsoft/apartment_parking_manager/data/repository/ApartmentRepository.java
@@ -3,8 +3,12 @@ package com.windsoft.apartment_parking_manager.data.repository;
 import com.windsoft.apartment_parking_manager.data.entity.Apartment;
 import com.windsoft.apartment_parking_manager.data.entity.id.ApartmentId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ApartmentRepository extends JpaRepository<Apartment, ApartmentId> {
 
     Apartment findByAddressAndStatus(String address, String status);
+
+    @Query("SELECT MAX(a.aptCode) FROM Apartment a")
+    String getMaxApartmentCode();
 }
