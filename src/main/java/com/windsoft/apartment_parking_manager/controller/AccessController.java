e71680e140af2e314130610fd0d14bebc81026ad
donghyun lee <80296014+donghyunlee-dev@users.noreply.github.com>
2026-02-13 14:32:21 +0900

    Add files via upload

--- src/main/java/com/windsoft/apartment_parking_manager/controller/AccessController.java
+++ src/main/java/com/windsoft/apartment_parking_manager/controller/AccessController.java
@@ -2,6 +2,8 @@ package com.windsoft.apartment_parking_manager.controller;
 
 import com.windsoft.apartment_parking_manager.data.dto.AccessRequestDto;
 import com.windsoft.apartment_parking_manager.data.dto.AccessResponseDto;
+import com.windsoft.apartment_parking_manager.data.dto.AdminRequestDto;
+import com.windsoft.apartment_parking_manager.data.dto.AdminResponseDto;
 import com.windsoft.apartment_parking_manager.service.AccessService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -12,15 +14,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RequestMapping("/api/access")
+@RequestMapping("/api")
 @RestController
 public class AccessController {
 
     private final AccessService accessService;
 
-    @PostMapping
+    @PostMapping("/access")
     public ResponseEntity<AccessResponseDto> validateAccess(@RequestBody AccessRequestDto request) {
         AccessResponseDto response = accessService.validateAccess(request);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping("/views/sign")
+    public ResponseEntity<AdminResponseDto> signIn(@RequestBody AdminRequestDto request) {
+
+        AdminResponseDto response = accessService.signIn(request);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
