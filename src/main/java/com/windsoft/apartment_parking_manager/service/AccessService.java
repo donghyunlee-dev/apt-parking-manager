141e4083ac5e7335b76001df2d66da8106627111
donghyun lee <80296014+donghyunlee-dev@users.noreply.github.com>
2026-02-13 14:35:32 +0900

    Add files via upload

--- src/main/java/com/windsoft/apartment_parking_manager/service/AccessService.java
+++ src/main/java/com/windsoft/apartment_parking_manager/service/AccessService.java
@@ -2,7 +2,21 @@ package com.windsoft.apartment_parking_manager.service;
 
 import com.windsoft.apartment_parking_manager.data.dto.AccessRequestDto;
 import com.windsoft.apartment_parking_manager.data.dto.AccessResponseDto;
+import com.windsoft.apartment_parking_manager.data.dto.AdminRequestDto;
+import com.windsoft.apartment_parking_manager.data.dto.AdminResponseDto;
 
 public interface AccessService {
+    /**
+     * 앱 인증
+     * @param requestDto 인증 정보
+     * @return 인증된 경비원 정보
+     */
     AccessResponseDto validateAccess(AccessRequestDto requestDto);
+
+    /**
+     * 웹 로그인
+     * @param requestDto 아파트명 검색, 관리자 핀번호
+     * @return 관리자 정보
+     */
+    AdminResponseDto signIn(AdminRequestDto requestDto);
 }
